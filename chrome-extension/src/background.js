4f74ace250f76d85758f4eed096cc6dbc86d2769
Jinesh Doshi <jinesh@giantconnect.com>
2023-07-30 00:05:58 -0700

    generate comic text using claud and generate a pdf from it

--- chrome-extension/src/background.js
+++ chrome-extension/src/background.js
@@ -1,127 +1,81 @@
 'use strict';
 
-// With background scripts you can communicate with popup
-// and contentScript files.
-// For more information on background script,
-// See https://developer.chrome.com/extensions/background_pages
-
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   if (request.type === 'GREETINGS') {
-//     const message = `Hi ${
-//       sender.tab ? 'Con' : 'Pop'
-//     }, my name is Bac. I am from Background. It's great to hear from you.`;
-//
-//     // Log message coming from the `request` parameter
-//     console.log(request.payload.message);
-//     // Send a response message
-//     sendResponse({
-//       message,
-//     });
-//   }
-// });
-
-// Main logic
-
-// chrome.runtime.onMessage.addListener((msg) => {
-//
-//   if (msg.generateComic) {
-//
-//     // Get options
-//     chrome.storage.sync.get(['age', 'style'], (options) => {
-//
-//       // Generate comic
-//       const comic = generateComic(options);
-//
-//       // Send back
-//       chrome.runtime.sendMessage({comic});
-//
-//     });
-//
-//   }
-//
-// });
-//
-// function generateComic(options) {
-//   // Use options to generate comic
-//   return comicHTML;
-// }
-
-// Comic generation logic
-
-// chrome.runtime.onMessage.addListener(async (msg) => {
-//
-//   if (msg.generateComic) {
-//     const {age, style} = msg.options;
-//     const text = await getPageText();
-//     const comic = await generateComic(text);
-//
-//     chrome.tabs.create({url: comic});
-//
-//   }
-//
-// });
-//
-// async function getPageText() {
-//   // Get text via content script
-// }
-//
-// async function generateComic(text) {
-//   // Use APIs to generate comic
-//   return comicURL;
-// }
-
 import Anthropic from '@anthropic-ai/sdk';
+console.log("Background script running...");
 
-console.log("Background script loaded");
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+async function getPageContent() {
+
+  var text;
+
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  });
+
+  const results = await chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    func: () => {
+      text = document.documentElement.innerText;
+      return {text: text}
+    }
+  });
+  return results[0].result.text;
+}
+
+chrome.runtime.onMessage.addListener(async function(request, sender, sendResponse) {
   console.log('Background script Message received:', request);
   if (request.action === "generateComic") {
     let age = request.age;
     let style = request.style;
 
-    // Fetch the content of the current tab
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      let currentTabId = tabs[0].id;
-      chrome.tabs.sendMessage(currentTabId, {action: "getContent"}, function(response) {
-        let content = response.content;
+    const pageContent = await getPageContent();
 
-        const userQuestion = `Please provide a summary of the above text to explain someone of age ${age} in a ${style} comic book style.`;
-        const prompt = `\n\n${content}\n\nHuman: ${userQuestion}\n\nAssistant:`;
+    const userQuestion = `Please generate a conversation between 2 comic characters with a summary of the above text to explain someone of age ${age} in a ${style} comic book style.`;
+    const prompt = `\n\n${pageContent}\n\nHuman: ${userQuestion}\n\nAssistant:`;
+    console.log(prompt);
 
-        // Call your server to generate the comic
-        // fetch('https://api.anthropic.com/v1/complete', {
-        //   method: 'POST',
-        //   body: JSON.stringify({content: prompt, age: age, style: style}),
-        //   headers: {
-        //     'Content-Type': 'application/json',
-        //     'anthropic-version': '2023-06-01',
-        //     "x-api-key": `"${process.env.ANTHROPIC_API_KEY}"`
-        //   }
-        // })
-        //   .then(response => response.blob())
-        //   .then(blob => {
-        //     // Create a new URL for the blob
-        //     let url = URL.createObjectURL(blob);
-        //
-        //     // Open the URL in a new tab
-        //     chrome.tabs.create({url: url});
-        //   });
-        chrome.storage.sync.get('apiKey', function(data) {
-          let apiKey = data.apiKey;
-          const anthropic = new Anthropic({ apiKey });
-          const completion = anthropic.completions.create({
-            model: 'claude-2',
-            max_tokens_to_sample: 100000,
-            prompt: prompt,
-          })
-            .then(response => response.completion)
-            .then(data => {
-              let url = URL.createObjectURL(data);
-              chrome.tabs.create({url: url});
-            });
+    chrome.storage.sync.get('AnthropicApiKey', async function (data) {
+      let AnthropicApiKey = data.AnthropicApiKey;
+      try {
+        const reqData = {
+          "model": "claude-2",
+          "prompt": prompt,
+          "max_tokens_to_sample": 100000
+        }
+        let response = await fetch('https://api.anthropic.com/v1/complete', {
+          method: 'POST',
+          body: JSON.stringify(reqData),
+          headers: {
+            'Content-Type': 'application/json',
+            'anthropic-version': '2023-06-01',
+            'x-api-key': AnthropicApiKey
+          }
         });
-      });
+
+        let data = await response.json();
+        // console.log("data: ", data.completion);
+        chrome.runtime.sendMessage({action: "generatePDF", data: data.completion});
+
+      } catch (error) {
+        console.error('Error:', error);
+      }
     });
+
+    // TODO: Error: Anthropic TS SDK client not initializing correctly. So we are using the REST API.
+    // chrome.storage.sync.get('apiKey', function(data) {
+    //   let apiKey = data.apiKey;
+    //   const anthropic = new Anthropic({apiKey: apiKey, authToken: ""});
+    //   const completion = anthropic.completions.create({
+    //     model: 'claude-2',
+    //     max_tokens_to_sample: 100000,
+    //     prompt: prompt,
+    //   })
+    //     .then(response => response.completion)
+    //     .then(data => {
+    //       let url = URL.createObjectURL(data);
+    //       chrome.tabs.create({url: url});
+    //     });
+    // });
   }
 });
 
