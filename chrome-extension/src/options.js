73dd9eeac6fd866b66da6535b1832827737c55a9
Jinesh Doshi <jinesh@giantconnect.com>
2023-07-30 03:10:55 -0700

    text to image generation for comic book

--- chrome-extension/src/options.js
+++ chrome-extension/src/options.js
@@ -2,15 +2,26 @@ document.getElementById('optionsForm').addEventListener('submit', function(event
   // Prevent the form from submitting normally
   event.preventDefault();
 
-  let AnthropicApiKey = document.getElementById('AnthropicApiKey').value;
+  let anthropicApiKey = document.getElementById('AnthropicApiKey').value;
+  let stabilityApiKey = document.getElementById('StabilityApiKey').value;
 
-  // Save the API key to the Chrome storage
-  chrome.storage.sync.set({AnthropicApiKey: AnthropicApiKey}, function() {
+  // Save the AnthropicApiKey to the Chrome storage
+  chrome.storage.sync.set({AnthropicApiKey: anthropicApiKey}, function() {
     alert('Options saved.');
   });
+
+  // Save the StabilityApiKey to the Chrome storage
+  chrome.storage.sync.set({StabilityApiKey: stabilityApiKey}, function() {
+    alert('Options saved.');
+  });
+
 });
 
 // Load the saved API key and populate the input field
 chrome.storage.sync.get('AnthropicApiKey', function(data) {
-  document.getElementById('AnthropicApiKey').value = data.apiKey;
+  document.getElementById('AnthropicApiKey').value = data.AnthropicApiKey;
+});
+
+chrome.storage.sync.get('StabilityApiKey', function(data) {
+  document.getElementById('StabilityApiKey').value = data.StabilityApiKey;
 });
